353d0f0a0bd4996cf11ec5b31964503767efe57e
oliver6312 <oliver.juhl.05@gmail.com>
2026-02-20 21:02:08 +0100

    Turn Changing Button

---
+++ UI.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var turn_label: Label = $Panel/TurnLabel
+@onready var next_turn_button: Button = $Panel/NextTurnButton
+
+func _ready() -> void:
+	next_turn_button.pressed.connect(_on_next_turn_pressed)
+	TurnState.turn_changed.connect(_on_turn_changed)
+
+	# Initialize label immediately
+	_on_turn_changed(TurnState.current_turn)
+
+func _on_next_turn_pressed() -> void:
+	TurnState.next_turn()
+
+func _on_turn_changed(new_turn: Faction.Type) -> void:
+	turn_label.text = "%s turn" % _turn_name(new_turn)
+
+func _turn_name(t: Faction.Type) -> String:
+	match t:
+		Faction.Type.ORC: return "Orc"
+		Faction.Type.ELF: return "Elf"
+		Faction.Type.DWARF: return "Dwarf"
+		_: return "Unknown"
