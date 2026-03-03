6ce71f4ea8f1bb672300d7d1a00285e97d0cdc8e
oliver6312 <oliver.juhl.05@gmail.com>
2026-03-03 20:17:11 +0100

    moretowns

--- Referee/Faction.gd
+++ Referee/Faction.gd
@@ -6,6 +6,6 @@ enum Type { NEUTRAL, ORC, ELF, DWARF }
 static func color_for(f: Type) -> Color:
 	match f:
 		Type.ORC: return Color("701705")      
-		Type.ELF: return Color("#609C07")      
+		Type.ELF: return Color("#006400")      
 		Type.DWARF: return Color("#092FC8")    
-		_: return Color("#636363")            
+		_: return Color("#000000")            
